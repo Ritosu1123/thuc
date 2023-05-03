209af1716ffd9e8d6023d6d5c7acf673c440bfce
ItsaMeChun <langtuvotinh1132000@gmail.com>
2023-05-03 15:33:45 +0700

    n+1

--- lib/detail/components/addtocart.dart
+++ lib/detail/components/addtocart.dart
@@ -12,48 +12,41 @@ class AddProductToCart extends StatefulWidget {
   @override
   _AddProductToCartState createState() => _AddProductToCartState();
 }
+
 class _AddProductToCartState extends State<AddProductToCart> {
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 50,
-      width: MediaQuery
-          .of(context)
-          .size
-          .width,
+      width: MediaQuery.of(context).size.width,
       child: ElevatedButton(
         onPressed: () {
           Cart cart = Cart();
           cart.addProductToCart(widget.product);
-          print(cart
-              .getCart()
-              .length
-              .toString());
+          print(cart.getCart().length.toString());
           Fluttertoast.showToast(
-              msg: "Add to cart",
+              msg: "Added to cart",
               toastLength: Toast.LENGTH_SHORT,
               gravity: ToastGravity.BOTTOM,
               timeInSecForIosWeb: 1,
               backgroundColor: Colors.red,
               textColor: Colors.white,
-              fontSize: 16.0
-          );
+              fontSize: 16.0);
         },
         style: ElevatedButton.styleFrom(
           shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10)),
+            borderRadius: BorderRadius.circular(10),
+          ),
           primary: Colors.green,
         ),
-        child: Text("Add to cart", style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Colors.white),),),
+        child: Text(
+          "Add to cart",
+          style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.white),
+        ),
+      ),
     );
   }
 }
